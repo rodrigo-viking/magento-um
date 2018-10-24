7b8c70f933e5cdfb94950589720d3d469dcc3243
TheViking <rodrigotonifreitas@gmail.com>
2018-10-24 17:43:10 -0300

    Ola mundo

---
+++ app/code/local/Academia/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+class Academia_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action{
+    
+    public function indexAction(){
+        echo "Hello World";
+    }
+
+    public function olaAction(){
+        echo "Ola";
+    }
+}
---
+++ app/code/local/FCamera/OlaMundo/Controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+class FCamara_OlaMundo_IndexController extends Mage_Core_Controller_Front_Action{
+    
+    public function indexAction(){
+        echo "Ola FC!";
+    }
+
+    public function HelloWorldAction(){
+        echo "EhNozes";
+    }
+}
